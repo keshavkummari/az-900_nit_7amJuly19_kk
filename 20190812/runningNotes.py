1bf992cf790cbad37946b152b3fc0afb4dc4d9d9
keshavkummari <keshav.kummari@gmail.com>
2019-08-12 09:28:17 +0530

    overview

---
+++ 20190812/runningNotes.py
@@ -0,0 +1,22 @@
+
+Agenda :
+
+1. The Azure Marketplace :
+
+1. An online applications and services marketplace with over 8000 listings.
+2. Offers technial solutions and service from Microsoft and partners that are designed to extend Azure Products and Services.
+3. Offers SaaS applications, Virtual Machines, Solution Templates, and Azure Managed Applications.
+4. Dicover, try and buy cloud software solutions built on or for Azure.
+
+
+2. Azure Management Tools :
+    1. CLI
+    2. Powershell
+    3. Azure Portal 
+    4. Azure Advisor 
+3. Azure Solutions
+    1. IOT
+    2. Big Data and Analytics
+    3. AI
+    4. Serverless Computing
+    5. Beneftis 
